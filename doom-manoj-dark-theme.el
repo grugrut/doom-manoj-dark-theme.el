de4368c4d87033e75d3212169800fb69d16ec7e4
grugrut <grugruglut+github@gmail.com>
2019-03-05 23:49:20 +0900

    Initial commit

---
+++ doom-manoj-dark-theme.el
@@ -0,0 +1,48 @@
+;;; doom-manoj-dark-theme.el --- A dark theme from Manoj for doom-theme
+;; Author: grugrut <grugruglut+github@gmail.com>
+;; URL:
+;; Version: 1.00
+;; Package-Requires: ((emacs-doom-themes "2.1.0"))
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'doom-themes)
+
+(def-doom-theme doom-manoj-dark
+  "A dark theme from Manoj for doom-theme"
+
+  ;; name     default  256 16
+  ((bg        black)
+   (fg        WhiteSmoke)
+
+   (grey       base4)
+   (red        red))
+
+  ;; --- extra faces ------------------------
+  ((hl-line :background "grey10" :foreground "Old Lace")
+   
+   ;; --- major-mode faces ------------------
+   )
+  
+  ;; --- extra variables --------------------
+  ;; ()
+  )
+
+(provide 'doom-manoj-dark-theme)
+
+;;; doom-manoj-dark-theme.el ends here
